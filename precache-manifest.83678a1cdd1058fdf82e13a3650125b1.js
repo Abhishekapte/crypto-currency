288e25a6c83796ae660155601f269fbcf03be820
Abhishek <abhishek.aapte@gmail.com>
2019-04-27 20:41:00 +0530

    Updates

---
+++ precache-manifest.83678a1cdd1058fdf82e13a3650125b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f13d1a7b491f3dca20d74785335dcd",
+    "url": "/crypto-currency/index.html"
+  },
+  {
+    "revision": "acae935f9f0e3daeb117",
+    "url": "/crypto-currency/static/css/main.c0e481ea.chunk.css"
+  },
+  {
+    "revision": "7c255a69b751224cecf0",
+    "url": "/crypto-currency/static/js/2.ab2d17df.chunk.js"
+  },
+  {
+    "revision": "acae935f9f0e3daeb117",
+    "url": "/crypto-currency/static/js/main.32964dd0.chunk.js"
+  },
+  {
+    "revision": "04d0b109d78f5eaac03f",
+    "url": "/crypto-currency/static/js/runtime~main.8c836b57.js"
+  },
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/crypto-currency/static/media/logo.5c0436d5.png"
+  }
+]);
